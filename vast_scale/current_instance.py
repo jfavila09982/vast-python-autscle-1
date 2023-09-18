54b5697cd2dfd269cc8b7a1a583d0b355a3aa87c
marco <marco.favila@media-meter.com>
2023-09-18 15:45:35 +0800

    add show user instance

---
+++ vast_scale/current_instance.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import subprocess
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv('VAST_API_KEY')
+#print(f'API Key: {api_key}')
+
+def show_instances(api_key):
+    command = ['../vast_cli/vast.py', 'show', 'instances', '--api-key', api_key, '--raw']
+    
+    try:
+        command_output = subprocess.run(command, capture_output=True, text=True)
+
+        
+
+        return command_output.stdout
+    except Exception as e:
+        return f"An error occured {e}"
+    
+
+def show_machines(api_key):
+    command = ['../vast_cli/vast.py', 'show', 'machines', '--api-key', api_key, '--raw']
+    
+    try:
+        command_output = subprocess.run(command, capture_output=True, text=True)
+        return command_output.stdout
+    except Exception as e:
+        return f"An error occured {e}"
+        
+print_instances = show_instances(api_key)
+#print_machines = show_machines(api_key)
+
+print(print_instances)
+#print(print_machines)
